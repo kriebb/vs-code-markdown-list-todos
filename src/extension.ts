a674f792ec456527f4f21cfeff873188d26ccbf0
Kristof Riebbels <kristof.riebbels@recomatics.com>
2017-05-18 09:50:58 +0200

    refactored to a more objected oriented way of working

--- src/extension.ts
+++ src/extension.ts
@@ -3,27 +3,16 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+class GenerateKeyWordList
+{
+    messagesCount = new Array<vscode.Disposable>();
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "listing-keywords" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        
-        // The code you place here will be executed every time your command is executed
-
-        let doc = vscode.window.activeTextEditor.document;
+    process = (editor: vscode.TextEditor ) => {
+        let doc = editor.document;
 
         let todoCounter:number = 0;
         for(let i = 0; i < doc.lineCount; i++)
         {
-            console.log("line "+ i)
             try
             {
                 let line:vscode.TextLine = doc.lineAt(i);
@@ -43,7 +32,40 @@ export function activate(context: vscode.ExtensionContext) {
             
         }
         
-        vscode.window.setStatusBarMessage("Count Todo's: "+todoCounter)
+        for(let i = 0 ; i < this.messagesCount.length; i++)
+        {
+            this.messagesCount[i].dispose();
+        }
+        
+        let disposable =  vscode.window.setStatusBarMessage("Count Todo's: "+todoCounter)
+
+        this.messagesCount.push(disposable);
+    }
+
+    dispose = () => {
+        for(let i = 0 ; i < this.messagesCount.length; i++)
+        {
+            this.messagesCount[i].dispose();
+        }
+    }
+}
+
+
+let  generateKeyWordList = new GenerateKeyWordList();
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+        
+        // The code you place here will be executed every time your command is executed
+
+        generateKeyWordList.process(vscode.window.activeTextEditor);
+    
 
     });
 
@@ -52,4 +74,6 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+    generateKeyWordList.dispose();
 }
+
